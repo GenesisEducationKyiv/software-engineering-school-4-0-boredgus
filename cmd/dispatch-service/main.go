2c095e044b7488b873d8aae38a4535c3d1f3f11c
Daryna Kharchenko <daha.kyiv@gmail.com>
2024-06-21 21:45:33 +0300

    chore: added comments for dispatch service

--- cmd/dispatch-service/main.go
+++ cmd/dispatch-service/main.go
@@ -24,6 +24,7 @@ func main() {
 	logger := config.InitLogger(env.Mode, config.WithProcess("dispatch-service"))
 	defer logger.Flush()
 
+	// connection to currency service server
 	currencyServiceConn, err := grpc.NewClient(
 		fmt.Sprintf("%s:%s", env.CurrencyServiceAddress, env.CurrencyServicePort),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
@@ -31,17 +32,14 @@ func main() {
 	utils.PanicOnError(err, "failed to connect to currency service grpc server")
 	defer currencyServiceConn.Close()
 
-	url := fmt.Sprintf("%s:%s", env.DispatchServiceAddress, env.DispatchServicePort)
-	lis, err := net.Listen("tcp", url)
-	utils.PanicOnError(err, fmt.Sprintf("failed to listen %s", url))
-
+	// connection to db
 	postgresqlDB := utils.Must(db.NewPostrgreSQL(
 		env.PostgreSQLConnString,
 		sql.PostgeSQLMigrationsUp(logger),
 	))
-
 	store := store.NewStore(postgresqlDB, db.IsPqError)
 
+	// creation of mailman
 	mailman := mailing.NewMailman(mailing.SMTPParams{
 		Host:     env.SMTPHost,
 		Port:     env.SMTPPort,
@@ -50,20 +48,25 @@ func main() {
 		Password: env.SMTPPassword,
 	})
 
+	// initialization of dispatch service server
 	serviceParams := &dispatch_service.DispatchServiceParams{
 		Store:           store,
 		Logger:          logger,
 		Mailman:         mailman,
 		CurrencyService: grpc_client.NewCurrencyServiceClient(currencyServiceConn),
 	}
-
 	dispatchServiceServer := dispatch_service.NewDispatchServiceServer(
 		dispatch_service.NewDispatchService(serviceParams),
 		logger,
 	)
 
+	// starting of grpc server
 	server := grpc.NewServer()
 	dispatch_grpc.RegisterDispatchServiceServer(server, dispatchServiceServer)
+
+	url := fmt.Sprintf("%s:%s", env.DispatchServiceAddress, env.DispatchServicePort)
+	lis, err := net.Listen("tcp", url)
+	utils.PanicOnError(err, fmt.Sprintf("failed to listen %s", url))
 
 	logger.Infof("dispatch service started at %s", url)
 	utils.PanicOnError(server.Serve(lis), "failed to serve")
