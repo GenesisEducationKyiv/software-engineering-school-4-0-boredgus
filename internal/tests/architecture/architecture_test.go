fca8006021f76448c3caf296b297439492aa4605
Daryna Kharchenko <daha.kyiv@gmail.com>
2024-06-27 17:44:59 +0300

    test: adjusted architecture tests

--- internal/tests/architecture/architecture_test.go
+++ internal/tests/architecture/architecture_test.go
@@ -8,65 +8,12 @@ import (
 
 func TestArchitectureDependencyViolationFor(t *testing.T) {
 	tests := []struct {
-		name                   string
-		basePackage            string
-		ignoredDependencies    []string
-		notAllowedDependencies []string
+		name                         string
+		basePackage                  string
+		ignoredDependencies          []string
+		notAllowedDependencies       []string
+		notAllowedDirectDependencies []string
 	}{
-		{
-			name:        "services",
-			basePackage: "subscription-api/internal/services",
-			notAllowedDependencies: []string{
-				"subscription-api/cmd/...",
-				"subscription-api/internal/controllers/...",
-				"subscription-api/internal/services/dispatch/grpc",
-				"subscription-api/internal/services/currency/grpc",
-			},
-		},
-		{
-			name:        "dispatch service",
-			basePackage: "subscription-api/internal/services/dispatch",
-			notAllowedDependencies: []string{
-				"subscription-api/cmd/...",
-				"subscription-api/internal/controllers/...",
-				"subscription-api/internal/services/dispatch/server",
-				"subscription-api/internal/services/dispatch/server/grpc",
-			},
-		},
-		{
-			name:        "dispatch service server",
-			basePackage: "subscription-api/internal/services/dispatch/server",
-			notAllowedDependencies: []string{
-				"subscription-api/cmd/...",
-				"subscription-api/internal/controllers/...",
-			},
-		},
-		{
-			name:        "currency service",
-			basePackage: "subscription-api/internal/services/currency",
-			notAllowedDependencies: []string{
-				"subscription-api/cmd/...",
-				"subscription-api/internal/controllers/...",
-				"subscription-api/internal/services/currency/server",
-				"subscription-api/internal/services/currency/server/grpc",
-			},
-		},
-		{
-			name:        "dispatch currency server",
-			basePackage: "subscription-api/internal/services/currency/server",
-			notAllowedDependencies: []string{
-				"subscription-api/cmd/...",
-				"subscription-api/internal/controllers/...",
-			},
-		},
-		{
-			name:        "controllers",
-			basePackage: "subscription-api/internal/controllers",
-			notAllowedDependencies: []string{
-				"subscription-api/cmd/...",
-				"subscription-api/internal/services/dispatch",
-			},
-		},
 		{
 			name:        "entities",
 			basePackage: "subscription-api/internal/entities",
@@ -82,6 +29,96 @@ func TestArchitectureDependencyViolationFor(t *testing.T) {
 				"subscription-api/internal/tests/...",
 				"subscription-api/internal/tests/...",
 			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
+		},
+		{
+			name:        "clients",
+			basePackage: "subscription-api/internal/clients/...",
+			notAllowedDependencies: []string{
+				"subscription-api/cmd/...",
+				"subscription-api/internal/controllers/...",
+				"subscription-api/internal/services/dispatch/grpc",
+				"subscription-api/internal/services/currency/grpc",
+			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
+		},
+		{
+			name:        "services",
+			basePackage: "subscription-api/internal/services",
+			notAllowedDependencies: []string{
+				"subscription-api/cmd/...",
+				"subscription-api/internal/controllers/...",
+				"subscription-api/internal/services/dispatch/grpc",
+				"subscription-api/internal/services/currency/grpc",
+			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
+		},
+		{
+			name:        "dispatch service",
+			basePackage: "subscription-api/internal/services/dispatch",
+			notAllowedDependencies: []string{
+				"subscription-api/cmd/...",
+				"subscription-api/internal/controllers/...",
+				"subscription-api/internal/services/dispatch/server",
+				"subscription-api/internal/services/dispatch/server/grpc",
+			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
+		},
+		{
+			name:        "dispatch service server",
+			basePackage: "subscription-api/internal/services/dispatch/server",
+			notAllowedDependencies: []string{
+				"subscription-api/cmd/...",
+				"subscription-api/internal/controllers/...",
+			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
+		},
+		{
+			name:        "currency service",
+			basePackage: "subscription-api/internal/services/currency",
+			notAllowedDependencies: []string{
+				"subscription-api/cmd/...",
+				"subscription-api/internal/controllers/...",
+				"subscription-api/internal/services/currency/server",
+				"subscription-api/internal/services/currency/server/grpc",
+			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
+		},
+		{
+			name:        "dispatch currency server",
+			basePackage: "subscription-api/internal/services/currency/server",
+			notAllowedDependencies: []string{
+				"subscription-api/cmd/...",
+				"subscription-api/internal/controllers/...",
+			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
+		},
+		{
+			name:        "controllers",
+			basePackage: "subscription-api/internal/controllers",
+			notAllowedDependencies: []string{
+				"subscription-api/cmd/...",
+				"subscription-api/internal/services/dispatch",
+				"google.golang.org/grpc",
+				"github.com/gin-gonic/gin",
+			},
+			notAllowedDirectDependencies: []string{
+				"go.uber.org/zap",
+			},
 		},
 	}
 
@@ -89,9 +126,11 @@ func TestArchitectureDependencyViolationFor(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			mockedT := new(testingT)
 
-			archtest.Package(mockedT, tt.basePackage).
-				Ignoring(tt.ignoredDependencies...).
-				ShouldNotDependOn(tt.notAllowedDependencies...)
+			packageTest := archtest.Package(mockedT, tt.basePackage).
+				Ignoring(tt.ignoredDependencies...)
+
+			packageTest.ShouldNotDependOn(tt.notAllowedDependencies...)
+			packageTest.ShouldNotDependDirectlyOn(tt.notAllowedDirectDependencies...)
 
 			mockedT.AssertNoError(t, mockedT)
 		})
