2c523b29659ca180dae14d53d096a4405f50b949
Daryna Kharchenko <daha.kyiv@gmail.com>
2024-07-16 13:46:27 +0300

    feat: added script to create multiple databases in one postgres instance

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -3,19 +3,31 @@
 set -e
 set -u
 
-function create_user_and_database() {
+function create_database() {
     local database=$1
-    echo "  Creating a database '$database'"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        CREATE DATABASE $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+
+    psql -v ON_ERROR_STOP=1 \
+        --username "$POSTGRES_USER" \
+        -c "SELECT 'found' FROM pg_database WHERE datname = '$database';" > "$database.txt"
+    if grep -q "found" "$database.txt"
+    then
+        echo "  Database '$database' already exists. Skipping..."
+    else 
+        echo "  Creating a database '$database'..."
+        psql -U postgres -v ON_ERROR_STOP=1 <<-EOSQL
+            CREATE DATABASE $database;
+            GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+            \connect $database;
+            CREATE SCHEMA $database;
+            ALTER SCHEMA $database OWNER TO $POSTGRES_USER;
 EOSQL
+    fi
 }
 
 if [ -n "$POSTGRES_DATABASES" ]; then
-    echo "Multiple database creation requested: $POSTGRE_DATABASES"
+    echo "Multiple database creation requested: $POSTGRES_DATABASES"
     for db in $(echo $POSTGRES_DATABASES | tr ',' ' '); do
-        create_user_and_database $db
+        create_database $db
     done
     echo "Multiple databases created"
 fi
